5e0a3a7155896d262c4e3b13e239b87092c58fec
Ant Cosentino <antony@mycosentino.com>
2015-07-20 10:35:08 +0200

    added docstrings to most functions removed some code duplication by adding callbacks to some functions and genericising others

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,11 @@
 
 'use strict';
 
+/**
+ *
+ * @param  {String} string
+ * @return {String}        html-entitied string
+ */
 function escapeHtml(string) {
   var entityMap = {
     '&': '&amp;',
@@ -29,57 +34,126 @@ function all(selector) {
   );
 }
 
-function appendMessage(message, done) {
+/**
+ * append a message to the chat stream
+ * @param  {Object}   message keyed by `sender` and `body`
+ * @param  {Function} next    to be executed after function is complete
+ * @return {Function}
+ */
+function appendMessage(message, next) {
   var div = document.createElement('div');
-  div.textContent = message.sender + ': ' + escapeHtml(message.body);
+  div.textContent = message.sender + ': ' + message.body;
   one('main').appendChild(div);
-  if (typeof done === 'function') return done();
+  if (typeof next === 'function') return next(message);
 }
 
-function appendNotification(notification, done) {
+/**
+ * append a message to the chat stream indicating
+ * that a client has joined the chat room
+ * @param  {Object}   who
+ * @param  {Function} next to be executed after the function is complete
+ * @return {Function}
+ */
+function announceJoined(who, next) {
   var div = document.createElement('div');
-
-  if (notification.joined) {
-    div.textContent = escapeHtml(notification.displayName + ' connected');
-  } else if (notification.parted) {
-    div.textContent = escapeHtml(notification.displayName + ' disconnected');
-  } else {
-    return;
-  }
-
+  div.textContent = who.displayname + ' connected';
   one('main').appendChild(div);
-  if (typeof done === 'function') return done();
+  if (typeof next === 'function') return next();
 }
 
-function clientList(clients, done) {
-  var select = one('#client-list');
-  for (var i in clients) {
-    var option = document.createElement('option');
-    option.value = clients[i].socket;
-    option.textContent = clients[i].displayName;
-    option.setAttribute('steam', clients[i].steam);
-    option.setAttribute('twitch', clients[i].twitch);
-    option.setAttribute('socket', clients[i].socket);
-    select.appendChild(option);
-  }
+/**
+ * append a message to the chat stream indicating
+ * that a client has left the chat room
+ * @param  {Object}   who
+ * @param  {Function} next to be executed after function is complete
+ * @return {Function}
+ */
+function announceLeft(who, next) {
+  var div = document.createElement('div');
+  div.textContent = who.displayname + ' disconnected';
+  one('main').appendChild(div);
+  if (typeof next === 'function') return next();
 }
 
-function emitMessage(message, done) {
-  socket.emit('chat message', {
-    sender: one('[data-displayname]').getAttribute('data-displayname'),
-    body: one('#message-input').value
-  });
-  if (typeof done === 'function') return done();
+/**
+ * construct a connected clients list from an array
+ * of client objects
+ * @param  {Array}   clients
+ * @param  {Function} next    to be executed after function is complete
+ * @return {Function}
+ */
+function clientList(clients, next) {
+  for (var i in clients) addClient(
+    clients[i], one('#client-list')
+  );
+  if (typeof next === 'function') return next();
 }
 
-function resetForm(done) {
+/**
+ * emits a chat message event (to the websocket server)
+ * containing a message object keyed by `sender` and `body`
+ * which is relayed to other connected clients.
+ *
+ * @param  {Object}   message
+ * @param  {Function} next    to be executed after function is complete
+ * @return {Function}
+ */
+function emitMessage(message, next) {
+  socket.emit('chat message', message);
+  if (typeof next === 'function') return next();
+}
+
+/**
+ * add a given option to the connected clients list
+ * @param {Object}   who  client object (socket, displayname, steam, twitch)
+ * @param {HTMLInputElement.select}   to   options are added to this element
+ * @param {Function} next   to be executed after function is complete
+ */
+function addClient(who, to, next) {
+  var select = to || one('#client-list');
+  var option = document.createElement('option');
+  option.id = who.socket;
+  option.textContent = who.displayname;
+  option.setAttribute('data-steam', who.steam);
+  option.setAttribute('data-twitch', who.twitch);
+  option.setAttribute('data-socket', who.socket);
+  select.appendChild(option);
+  if (typeof next === 'function') return next(who);
+}
+
+/**
+ * remove a given option from the connected clients list
+ * @param  {Object}   who  client object (socket, displayname, steam, twitch)
+ * @param  {Function} next to be executed after function is complete
+ * @return {Function}
+ */
+function removeClient(who, next) {
+  var option = one('#' + who.socket);
+  option.remove();
+  if (typeof next === 'function') return next(who);
+}
+
+/**
+ * resets the message submission form
+ * @param  {Function} next executed after function is complete
+ * @return {Function}
+ */
+function resetForm(next) {
   one('#message-form').reset();
+  if (typeof next === 'function') return next();
 }
 
-var socket = io();
-var user = one('#user');
-
-one('#message-form').onsubmit = function(e) {
+/**
+ * wrap up the message string from textarea into an object
+ * keyed by `sender` and `body`. immediately append the message
+ * string to the message log and only then emit a socket event
+ * containing the message object to be relayed (via websocket server)
+ * to other connected clients.
+ *
+ * @param  {CustomEvent} e preventable, propagation stoppable
+ * @return {Function}
+ */
+function messageFormSubmit(e) {
   e.preventDefault();
   var message = {
     sender: one('[data-displayname]').getAttribute('data-displayname'),
@@ -88,28 +162,22 @@ one('#message-form').onsubmit = function(e) {
   return message.body.length ? appendMessage(
     message, emitMessage.bind(this, message, resetForm)
   ) : undefined;
-};
+}
+
+var socket = io();
+var user = one('#user');
+
+one('#message-form').onsubmit = messageFormSubmit;
 
 socket.on('client list', clientList);
 socket.on('chat message', appendMessage);
 socket.on('someone joined', function(who) {
-  appendNotification(who, function() {
-    var select = one('#client-list');
-    var option = document.createElement('option');
-    option.value = who.socket;
-    option.textContent = who.displayName;
-    option.setAttribute('steamid', who.steam);
-    option.setAttribute('twitch', who.twitch);
-    option.setAttribute('socketid', who.socket);
-    select.appendChild(option);
-  });
+  addClient(who, one('#client-list'), announceJoined);
 });
 socket.on('someone left', function(who) {
-  appendNotification(who, function() {
-    var option = one('option[value="' + who.socket + '"]');
-    option.remove();
-  });
+  removeClient(who, announceLeft);
 });
+
 socket.emit(
   'identify',
   user.getAttribute('data-steam'),
