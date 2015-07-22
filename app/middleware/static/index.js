d6be4594e72e7ed350f856987f5908d2394b01d2
Ant Cosentino <antony@mycosentino.com>
2015-07-22 20:09:42 +0200

    cleared up static middleware

--- app/middleware/static/index.js
+++ app/middleware/static/index.js
@@ -1,12 +1,9 @@
 
-var favicon = require('serve-favicon');
-
 var FAVICON = process.env.FAVICON;
 var STATIC = process.env.STATIC;
-var PORT = process.env.PORT;
 
 module.exports = function() {
 
   this.use(this.get('express.static')(STATIC));
-  this.use(favicon(FAVICON));
+  this.use(require('serve-favicon')(FAVICON));
 };
