17de731c02c8c6b09d0a8064fc10571961b325e2
Ant Cosentino <antony@mycosentino.com>
2015-07-22 22:15:31 +0200

    fixed user update route fixed delete route

--- app/routes/user/index.js
+++ app/routes/user/index.js
@@ -3,43 +3,52 @@
 module.exports = function() {
 
   this.put('/user/:userid', function(req, res) {
-    this.get('users.findOne', {
-      deathmatch_id: req.body.deathmatch
+    this.get('users.findOne')({
+      _id: req.params.userid
     }, function(err, user) {
-      if (err) throw err; // res.status 500 and json error object
-      if (user._id === req.params.userid) {
-        this.get('users.updateOne')({
-          _id: req.params.userid
-        }, {
-          email: req.body.email,
-          display_name: req.body.displayname
-        }, function(err, user) {
-          if (err) throw err; // res.status 500 and json error
-          res.status(200).json({
-            error: false
-          });
+      if (err) return res.status(500).json(err);
+      if (user.deathmatch_id !== parseInt(req.body.deathmatch, 10)) {
+        return res.status(401).json({
+          error: true,
+          message: 'unauthorised'
         });
       }
+      this.get('users.updateOne')({
+        _id: req.params.userid
+      }, {
+        $set: {
+          email: req.body.email,
+          display_name: req.body.displayname
+        }
+      }, function(err, user) {
+        if (err) return res.status(500).json(err);
+        res.status(200).json({
+          error: false,
+          user: user
+        });
+      });
     }.bind(this));
-
   }.bind(this));
 
   this.delete('/user/:userid', function(req, res) {
-    this.get('users.findOne', {
-      deathmatch_id: req.body.deathmatch
+    this.get('users.findOne')({
+      _id: req.params.userid
     }, function(err, user) {
-      if (err) throw err; // res.status 500 and json error object
-      if (user._id === req.params.userid) {
-        this.get('users.deleteOne')({
-          _id: req.params.userid
-        }, function(err, user) {
-          if (err) throw err; // res.status 500 and json error
-          res.status(200).json({
-            error: false
-          });
+      if (err) res.status(500).json(err);
+      if (user.deathmatch_id !== parseInt(req.body.deathmatch, 10)) {
+        return res.status(401).json({
+          error: true,
+          message: 'unauthorised'
         });
       }
+      this.get('users.deleteOne')({
+        _id: req.params.userid
+      }, function(err, user) {
+        if (err) res.status(500).json(err);
+        res.status(200).json({
+          error: false
+        });
+      });
     }.bind(this));
   }.bind(this));
-
 };
