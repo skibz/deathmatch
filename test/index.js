f336294562f59f7b9a078ada831ff0afe5c95e34
Ant Cosentino <antony@mycosentino.com>
2015-07-25 20:09:09 +0200

    added test cases for lobby additions

--- test/index.js
+++ test/index.js
@@ -34,7 +34,7 @@ describe('lobby class', function() {
       expect(lobby._finaliser).to.be.null;
       expect(lobby._createdAt).to.be.greaterThan(0);
 
-      describe('defauls', function() {
+      describe('defaults', function() {
         var lobby = Lobby.create({});
         it('should initialise with default instance properties if no options are given', function() {
           expect(lobby._players).to.deep.equal({});
@@ -61,11 +61,7 @@ describe('lobby class', function() {
 
     describe('properties', function() {
       describe('_players', function() {
-        var lobby = Lobby.create({
-          players: {},
-          map: 'abc',
-          format: 2
-        });
+        var lobby = Lobby.create();
         it('should be empty after initialisation', function() {
           expect(lobby._players).to.deep.equal({});
         });
@@ -77,73 +73,52 @@ describe('lobby class', function() {
           lobby.rem('abc');
           expect(lobby.players()).to.not.contain('abc');
         });
-
-        describe('isAdded()', function() {
-          var lobby = Lobby.create({
-            players: {abcd: 'abcd'},
-            map: 'abc',
-            format: 2
-          });
-          it('should contain a property after initialisation', function() {
-            expect(lobby.isAdded('abcd')).to.equal(true);
-          });
-          it('should return false for a non existent property', function() {
-            expect(lobby.isAdded('lkjasdflksdabcd')).to.equal(false);
-          });
-
-          describe('isFull()', function() {
-            var lobby = Lobby.create({
-              players: {abcd: 'abcd'},
-              map: 'abc',
-              format: 2
-            });
-            it('should return false if the number of player properties is less than twice the value of _format', function() {
-              expect(lobby.isFull()).to.equal(false);
-            });
-          });
+      });
+      describe('isAdded()', function() {
+        var lobby = Lobby.create({players: {abcd: 'abcd'}});
+        it('should contain a property after initialisation', function() {
+          expect(lobby.isAdded('abcd')).to.equal(true);
+        });
+        it('should return false for a non existent property', function() {
+          expect(lobby.isAdded('lkjasdflksdabcd')).to.equal(false);
+        });
+      });
+      describe('isFull()', function() {
+        var lobby = Lobby.create({format: 2});
+        it('should return false if the number of player properties is less than twice the value of _format', function() {
+          expect(lobby.isFull()).to.equal(false);
+        });
+        it('should return true if the number of player properties is twice the value of _format', function() {
+          lobby.add('1');
+          lobby.add('2');
+          lobby.add('3');
+          lobby.add('4');
+          expect(lobby.isFull()).to.equal(true);
         });
       });
       describe('_map', function() {
-        var lobby = Lobby.create({
-          players: {},
-          map: 'abc',
-          format: 2
-        });
+        var lobby = Lobby.create();
         it('should change after map() is invoked', function() {
           lobby.map('def');
           expect(lobby.map()).to.equal('def');
         });
       });
       describe('_format', function() {
-        var lobby = Lobby.create({
-          players: {},
-          map: 'abc',
-          format: 2
-        });
+        var lobby = Lobby.create({format: 2});
         it('should change after format() is invoked', function() {
           lobby.format(5);
           expect(lobby.format()).to.equal(10);
         });
       });
       describe('_timeout', function() {
-        var lobby = Lobby.create({
-          players: {},
-          map: 'abc',
-          format: 2,
-          timeout: 100
-        });
+        var lobby = Lobby.create({timeout: 100});
         it('should be equal to the given value if provided at construction time', function() {
           expect(lobby._timeout).to.equal(100);
         });
       });
       describe('_finalising', function() {
         it('should be false until total players is equal to double the value of _format', function() {
-          var lobby = Lobby.create({
-            players: {},
-            map: 'abc',
-            format: 2,
-            timeout: 100
-          });
+          var lobby = Lobby.create({format: 2});
           lobby.add('1');
           expect(lobby._finalising).to.equal(false);
           lobby.add('2');
@@ -154,12 +129,7 @@ describe('lobby class', function() {
           expect(lobby._finalising).to.equal(true);
         });
         it('should remain true if total players becomes less than double the value of _format after becoming double the value of _format', function() {
-          var lobby = Lobby.create({
-            players: {},
-            map: 'abc',
-            format: 2,
-            timeout: 100
-          });
+          var lobby = Lobby.create({format: 2});
           lobby.add('1');
           expect(lobby._finalising).to.equal(false);
           lobby.add('2');
@@ -176,13 +146,7 @@ describe('lobby class', function() {
       });
       describe('_finaliser', function() {
         it('should be null until total players is equal to double the value of _format', function() {
-          var lobby = Lobby.create({
-            players: {},
-            map: 'abc',
-            format: 2,
-            timeout: 100,
-            afterTimeout: function() {}
-          });
+          var lobby = Lobby.create({format: 2});
           expect(lobby._finaliser).to.equal(null);
           lobby.add('1');
           expect(lobby._finaliser).to.equal(null);
@@ -194,26 +158,33 @@ describe('lobby class', function() {
           expect(lobby._finaliser).to.be.an.instanceof(Object);
         });
       });
-      describe('_afterTimeout', function() {
+      describe('_starting', function() {
         it('should be invoked after _timeout milliseconds once total players is equal to double the value of _format', function(done) {
-          // this test case could be a little dicey...
-          //
-          // i'm passing the done callback into `afterTimeout`
-          // which will cause the test to fail if it's never
-          // invoked.
-          //
           var lobby = Lobby.create({
-            players: {},
-            map: 'abc',
             format: 1,
             timeout: 25,
-            afterTimeout: done
+            starting: done,
+            postponed: done
           });
-          expect(lobby._afterTimeout).to.be.an.instanceof(Function);
           lobby.add('1');
           expect(lobby._finaliser).to.equal(null);
           lobby.add('2');
           expect(lobby._finaliser).to.not.equal(null);
+        });
+      });
+      describe('_postponed', function() {
+        it('should be invoked after _timeout milliseconds once total players is equal to double the value of _format and subsequently becomes less than double the value of _format', function(done) {
+          var lobby = Lobby.create({
+            format: 1,
+            timeout: 25,
+            starting: done,
+            postponed: done
+          });
+          lobby.add('1');
+          expect(lobby._finaliser).to.equal(null);
+          lobby.add('2');
+          expect(lobby._finaliser).to.not.equal(null);
+          lobby.rem('2');
         });
       });
     });
