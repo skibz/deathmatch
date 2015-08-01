402157c8863de7c2470de95b001e5cce1e5fa349
Ant Cosentino <antony@mycosentino.com>
2015-08-01 21:34:34 +0200

    new implementation of rcon class using commands buffer and fluent api for building command buffer. safer approach as execution of the buffer can be triggered as a callback to a lobby starting. also far less leaky than original implementation which connected to the server at construction time and passed a reference to the rcon socket back to the calling context (probably leaving it unable to be garbage collected)

--- app/pickups/rcon.js
+++ app/pickups/rcon.js
@@ -7,56 +7,40 @@ var prefixer = require('../etc/prefixer');
 module.exports = {
   prototype: {
     map: function(map) {
-      this._context.exec(['sm_map', map].join(' '));
+      this._commands.push('sm_map ' + map);
+      return this;
     },
     say: function(message) {
-      this._context.exec(['sm_say', message].join(' '));
+      this._commands.push('sm_say ' + message);
+      return this;
     },
     kick: function(player, reason) {
-      this._context.exec(['sm_kick', player, reason].join(' '));
+      this._commands.push('sm_kick ' + player + ' ' + reason);
+      return this;
     },
     ban: function(player, duration, reason) {
-      this._context.exec(['sm_ban', player, duration, reason].join(' '));
+      this._commands.push('sm_ban ' + player + ' ' + duration + ' ' + reason);
+      return this;
     },
     unban: function(player) {
-      this._context.exec(['sm_unban', player].join(' '));
+      this._commands.push('sm_unban ' + player);
+      return this;
     },
-    exec: function(command) {
-      this._context.exec(command);
-    },
-    disconnect: function() {
-      this._context.close();
+    go: function() {
+      var rcon = new SimpleRcon(this._host, this._port, this._rcon);
+      rcon.on('error', function(err) {
+        throw new Error(err);
+      }).once('authenticated', rcon.exec.bind(
+        rcon, this._commands.join(';'), rcon.close.bind(rcon)
+      ));
     }
   },
-  create: function(options, done) {
-
-    // ought to change this class
-    // to accept a list of commands
-    // that are executed once the
-    // authenticated event is fired
-
-    var properties = prefixer(options);
-    var rcon = new SimpleRcon(
-      properties._host.value,
-      properties._port.value,
-      properties._rcon.value
+  create: function(options) {
+    options = options || {};
+    options.commands = [];
+    return Object.create(
+      module.exports.prototype,
+      prefixer(options)
     );
-
-    // this could be the leakiest
-    // code i have ever written...
-    rcon.on('error', function(err) {
-      console.error(new Date(), 'rcon error', err);
-    }).once('authenticated', function() {
-      properties._context = {
-        value: rcon,
-        writable: false,
-        enumerable: true,
-        configurable: true
-      };
-      return Object.create(
-        module.exports.prototype,
-        properties
-      );
-    });
   }
 };
