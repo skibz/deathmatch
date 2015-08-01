a99a68381e2596f906ed7056dae38e227cac5c79
Ant Cosentino <antony@mycosentino.com>
2015-08-01 21:35:04 +0200

    added first set of tests for rcon class

--- test/index.js
+++ test/index.js
@@ -6,6 +6,7 @@ chai.use(require('sinon-chai'));
 var expect = chai.expect;
 
 var Lobby = require('../app/pickups/lobby');
+var Rcon = require('../app/pickups/rcon');
 var prefixer = require('../app/etc/prefixer');
 
 describe('lobby class', function() {
@@ -19,12 +20,6 @@ describe('lobby class', function() {
   });
 
   describe('lobby instance', function() {
-
-    it('should throw an error if protected properties are used at construction time', function() {
-      expect(Lobby.create.bind(Lobby, {createdAt: true})).to.throw();
-      expect(Lobby.create.bind(Lobby, {starting: true})).to.throw();
-      expect(Lobby.create.bind(Lobby, {starter: true})).to.throw();
-    });
 
     it('should have instance properties', function() {
       var lobby = Lobby.create({
@@ -225,4 +220,113 @@ describe('prefixer', function() {
       enumerable: true
     });
   });
+});
+
+describe('rcon class', function() {
+
+  it('should have a prototype', function() {
+    expect(Rcon.prototype).to.be.an.instanceof(Object);
+  });
+
+  it('should have a create function', function() {
+    expect(Rcon.create).to.be.an.instanceof(Function);
+  });
+
+  describe('rcon instance', function() {
+
+    it('should have instance properties', function() {
+      var rcon = Rcon.create();
+      expect(rcon._commands).to.be.an.instanceof(Array);
+      expect(rcon._commands).to.be.empty;
+    });
+
+    it('should have instance functions', function() {
+      var rcon = Rcon.create();
+      expect(rcon.map).to.be.an.instanceof(Function);
+      expect(rcon.say).to.be.an.instanceof(Function);
+      expect(rcon.kick).to.be.an.instanceof(Function);
+      expect(rcon.ban).to.be.an.instanceof(Function);
+      expect(rcon.unban).to.be.an.instanceof(Function);
+      expect(rcon.go).to.be.an.instanceof(Function);
+    });
+
+    describe('fluent api', function() {
+      describe('map', function() {
+        it('should return itself', function() {
+          var rcon = Rcon.create();
+          expect(rcon.map('cp_badlands')).to.deep.equal(rcon);
+        });
+        it('should append a string element to the _commands array', function() {
+          var rcon = Rcon.create();
+          expect(rcon._commands.length).to.equal(0);
+          expect(rcon.map('cp_badlands')).to.deep.equal(rcon);
+          expect(rcon._commands.length).to.equal(1);
+        });
+      });
+      describe('say', function() {
+        it('should return itself', function() {
+          var rcon = Rcon.create();
+          expect(rcon.say('hello everyone')).to.deep.equal(rcon);
+        });
+        it('should append a string element to the _commands array', function() {
+          var rcon = Rcon.create();
+          expect(rcon._commands.length).to.equal(0);
+          expect(rcon.map('cp_badlands')).to.deep.equal(rcon);
+          expect(rcon._commands.length).to.equal(1);
+        });
+      });
+      describe('kick', function() {
+        it('should return itself', function() {
+          var rcon = Rcon.create();
+          expect(rcon.kick('zoidberg')).to.deep.equal(rcon);
+        });
+        it('should append a string element to the _commands array', function() {
+          var rcon = Rcon.create();
+          expect(rcon._commands.length).to.equal(0);
+          expect(rcon.map('cp_badlands')).to.deep.equal(rcon);
+          expect(rcon._commands.length).to.equal(1);
+        });
+      });
+      describe('ban', function() {
+        it('should return itself', function() {
+          var rcon = Rcon.create();
+          expect(rcon.ban('zoidberg')).to.deep.equal(rcon);
+        });
+        it('should append a string element to the _commands array', function() {
+          var rcon = Rcon.create();
+          expect(rcon._commands.length).to.equal(0);
+          expect(rcon.map('cp_badlands')).to.deep.equal(rcon);
+          expect(rcon._commands.length).to.equal(1);
+        });
+      });
+      describe('unban', function() {
+        it('should return itself', function() {
+          var rcon = Rcon.create();
+          expect(rcon.unban('zoidberg')).to.deep.equal(rcon);
+        });
+        it('should append a string element to the _commands array', function() {
+          var rcon = Rcon.create();
+          expect(rcon._commands.length).to.equal(0);
+          expect(rcon.map('cp_badlands')).to.deep.equal(rcon);
+          expect(rcon._commands.length).to.equal(1);
+        });
+      });
+    });
+
+    describe('go', function() {
+      it('should throw if no host, port and/or password are given', function() {
+        var rcon = Rcon.create();
+        expect(rcon.map('123').go).to.throw(TypeError);
+      });
+      it('should throw if simple-rcon emits an error', function() {
+        var rcon = Rcon.create({
+          host: 'localhost',
+          port: 27000,
+          rcon: 'abc123'
+        });
+        expect(rcon.go).to.throw(Error);
+      });
+    });
+  });
+
 });
