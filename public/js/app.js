525e271e115f36daceb2a18303672d4ba1c3dbc6
Ant Cosentino <antony@mycosentino.com>
2015-08-16 12:02:32 +0200

    removed comments

--- public/js/app.js
+++ public/js/app.js
@@ -13,11 +13,6 @@ function throttle(callback, limit) {
   }
 }
 
-/**
- * filters any dangerous characters
- * @param  {String} string
- * @return {String}        html-entitied string
- */
 function escapeHtml(string) {
   var entityMap = {
     '&': '&amp;',
@@ -46,12 +41,6 @@ function all(selector) {
   );
 }
 
-/**
- * append a message to the chat stream
- * @param  {Object}   message keyed by `sender` and `body`
- * @param  {Function} next    to be executed after function is complete
- * @return {Function}
- */
 function appendMessage(message, next) {
   var div = document.createElement('div');
   div.textContent = message.sender + ': ' + message.body;
@@ -59,13 +48,6 @@ function appendMessage(message, next) {
   if (typeof next === 'function') return next(message);
 }
 
-/**
- * append a message to the chat stream indicating
- * that a client has joined the chat room
- * @param  {Object}   who
- * @param  {Function} next to be executed after the function is complete
- * @return {Function}
- */
 function announceJoined(who, next) {
   var div = document.createElement('div');
   div.textContent = who.displayname + ' connected';
@@ -73,13 +55,6 @@ function announceJoined(who, next) {
   if (typeof next === 'function') return next();
 }
 
-/**
- * append a message to the chat stream indicating
- * that a client has left the chat room
- * @param  {Object}   who
- * @param  {Function} next to be executed after function is complete
- * @return {Function}
- */
 function announceLeft(who, next) {
   var div = document.createElement('div');
   div.textContent = who.displayname + ' disconnected';
@@ -87,13 +62,6 @@ function announceLeft(who, next) {
   if (typeof next === 'function') return next();
 }
 
-/**
- * construct a connected clients list from an array
- * of client objects
- * @param  {Array}   clients
- * @param  {Function} next    to be executed after function is complete
- * @return {Function}
- */
 function clientList(clients, next) {
   for (var client in clients) addClient(
     clients[client], one('#client-list')
@@ -101,26 +69,11 @@ function clientList(clients, next) {
   if (typeof next === 'function') return next();
 }
 
-/**
- * emits a chat message event (to the websocket server)
- * containing a message object keyed by `sender` and `body`
- * which is relayed to other connected clients.
- *
- * @param  {Object}   message
- * @param  {Function} next    to be executed after function is complete
- * @return {Function}
- */
 function emitMessage(message, next) {
   socket.emit('chat#message', message);
   if (typeof next === 'function') return next();
 }
 
-/**
- * add a given option to the connected clients list
- * @param {Object}   who  client object (socket, displayname, steam, twitch)
- * @param {HTMLInputElement.select}   to   options are added to this element
- * @param {Function} next   to be executed after function is complete
- */
 function addClient(who, to, next) {
   var select = to || one('#client-list');
   var option = document.createElement('option');
@@ -133,12 +86,6 @@ function addClient(who, to, next) {
   if (typeof next === 'function') return next(who);
 }
 
-/**
- * remove a given option from the connected clients list
- * @param  {Object}   who  client object (socket, displayname, steam, twitch)
- * @param  {Function} next to be executed after function is complete
- * @return {Function}
- */
 function removeClient(who, next) {
   all('#' + (who.socket || 'id_' + who.id)).forEach(function(o) {
     o.remove();
@@ -146,11 +93,6 @@ function removeClient(who, next) {
   if (typeof next === 'function') return next(who);
 }
 
-/**
- * resets the message submission form
- * @param  {Function} next executed after function is complete
- * @return {Function}
- */
 function resetForm(next) {
   one('#message-form').reset();
   if (typeof next === 'function') return next();
@@ -158,7 +100,7 @@ function resetForm(next) {
 
 var socket = io();
 var user = one('#user');
-var permission; // notifications
+var permission;
 
 socket.on('client#identity', function() {
 
