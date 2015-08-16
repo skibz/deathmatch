b8eba815804c9bf41b5604c667bd9f0a874188e7
Ant Cosentino <antony@mycosentino.com>
2015-08-16 02:14:15 +0200

    fixed disconnected user type error added lobby events removed uneeded auth stuff hey ho lets go

--- app/ws/index.js
+++ app/ws/index.js
@@ -5,73 +5,88 @@ var Lobby = require('../pickups/lobby');
 var Rcon = require('../pickups/rcon');
 
 var io;
-var clients = {}
-var auth = {};
+var clients = {};
 var lobby;
-var users;
+var servers, admins;
 
 module.exports = function() {
 
   io = require('socket.io')(this.http);
-  users = this.express.get('users.findOne');
+  servers = this.express.get('lobby.servers');
+  admins = this.express.get('lobby.admins');
 
   lobby = Lobby.create({
-    started: io.emit.bind(io, 'lobby#started'),
-    postponed: io.emit.bind(io, 'lobby#postponed')
+    server: servers.mweb1,
+    format: 6,
+    timeout: 60000,
+    started: function() {
+      io.sockets.emit('lobby#started', {
+        connect: 'steam://connect/' +
+          lobby._server.host + ':' +
+          lobby._server.port + '/' +
+          lobby._server.password
+      });
+    },
+    postponed: function() {
+      io.sockets.emit('lobby#postponed');
+    }
   });
 
   io.on('connection', function(socket) {
 
-    socket.on('identify', function(who) {
-      auth[socket.id] = who.deathmatch;
+    socket.emit('client#identity');
+
+    socket.on('client#identify', function(who) {
       clients[socket.id] = {
         socket: socket.id,
         displayname: who.displayname,
-        // twitch: who.twitch,
+        twitch: who.twitch,
         steam: who.steam
       };
 
-      socket.emit('client list', Object.keys(clients).map(function(client) {
+      socket.emit('client#list', Object.keys(clients).map(function(client) {
         return clients[client];
       }));
 
-      socket.broadcast.emit('someone joined', clients[socket.id]);
+      socket.broadcast.emit('chat#joined', clients[socket.id]);
     });
 
-    socket.on('chat message', function(message) {
-      socket.broadcast.emit('chat message', message);
+    socket.on('chat#message', function(message) {
+      socket.broadcast.emit('chat#message', message);
     });
 
-    socket.on('public add player', function() {
-      socket.join('lobby', function(err) {
-        console.error('public add player', err);
-        var player = clients[socket.id].displayname;
-        lobby.add(player);
-        io.emit('lobby#add', player);
+    socket.on('lobby#player-add', function() {
+      var player = clients[socket.id].displayname;
+      lobby.add(player);
+      io.emit('lobby#add', {
+        displayname: player,
+        id: clients[socket.id].steam || clients[socket.id].twitch
       });
     });
 
-    socket.on('public rem player', function() {
-      socket.leave('lobby', function(err) {
-        console.error('public rem player', err);
-        var player = clients[socket.id].displayname;
-        lobby.rem(player);
-        io.emit('lobby#rem', player);
+    socket.on('lobby#player-rem', function() {
+      var player = clients[socket.id].displayname;
+      lobby.rem(player);
+      io.emit('lobby#rem', {
+        displayname: player,
+        id: clients[socket.id].steam || clients[socket.id].twitch
       });
     });
 
     socket.on('error', console.error.bind(console));
 
     socket.on('disconnect', function() {
-      io.emit('someone left', clients[socket.id]);
-      // remove client from lobby if added
-      var player = clients[socket.id].displayname;
+      var client = clients[socket.id];
+      io.emit('chat#left', client);
+      var player = client.displayname;
       if (lobby.isAdded(player)) {
         lobby.rem(player);
-        io.emit('lobby#rem', player);
+        io.emit('lobby#rem', {
+          displayname: player,
+          id: clients[socket.id].steam || clients[socket.id].twitch
+        });
       }
       delete clients[socket.id];
-      delete auth[socket.id];
     });
 
   });
