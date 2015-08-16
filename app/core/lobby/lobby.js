a532a3deeb3ed60ed4abc4fcada0997d1c190c95
Ant Cosentino <antony@mycosentino.com>
2015-08-16 11:30:51 +0200

    removed stuff has been replaced by env vars and updated other classes to use env vars

--- app/core/lobby/lobby.js
+++ app/core/lobby/lobby.js
@@ -62,8 +62,8 @@ module.exports = {
     options = options || {};
 
     if (!('timeout' in options)) options.timeout = 60000;
-    if (!('map' in options)) options.map = 'cp_badlands';
-    if (!('server' in options)) options.server = 'MWEB_1';
+    if (!('map' in options)) options.map = process.env.DEFAULT_MAP;
+    if (!('server' in options)) options.server = process.env.DEFAULT_SERVER;
     if (!('format' in options)) options.format = 6;
     if (!('players' in options)) options.players = {};
     if (!('started' in options)) options.started = function() {};
